09e39c6c76eeedd11478c6cfebc0985e537d68f1
nagisa <1434936049@qq.com>
2023-04-11 16:51:59 +0800

    refactor:merge run_LFM() and run_KNN() to enter()

--- djangoProject/djangoProject/LFM.py
+++ djangoProject/djangoProject/LFM.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
-
+import os
 import pandas as pd
-from surprise import Reader, Dataset, dataset, SVD
+import surprise
+from surprise import Reader, Dataset, dataset, SVD, KNNBasic
 
 from djangoProject import RatingAPI
 from djangoProject import recommendAPI
-
-model = None
+from djangoProject import SimilarMovieAPI
 
 
 def get_top_n(predictions, n):
@@ -36,34 +36,54 @@ def get_top_n(predictions, n):
     return top_n
 
 
-def run_LFM():
-    print("run LFM")
+def enter():
+    print("run")
     data_from_sql = RatingAPI.select_all()
     data_list = list()
     user_set = set()
     movie_set = set()
     for x in data_from_sql:
-        data_list.append({'userId': x.userId, 'movieId': x.movieId, 'rating': x.rating})
-        user_set.add(x.userId)
-        movie_set.add(x.movieId)
+        data_list.append({'userId': x.user_id, 'movieId': x.movie_id, 'rating': x.rating})
+        user_set.add(x.user_id)
+        movie_set.add(x.movie_id)
+    print('movie_num:', len(movie_set))
     df = pd.DataFrame(data_list)
     reader = Reader(rating_scale=(1, 5), line_format='user item rating timestamp')
     data = Dataset.load_from_df(df, reader)
     train_set = dataset.DatasetAutoFolds.build_full_trainset(data)
-    global model
+    run_LFM(train_set)
+    run_KNN(train_set, movie_set)
+
+
+def run_LFM(train_set):
     model = SVD()
     model.fit(train_set)
     test_set = train_set.build_anti_testset()
     prediction = model.test(test_set)
     top_n = get_top_n(prediction, n=10)
     recommend_records = list()
-    # print(top_n)
     for user_id in top_n:
-        for movie_id, _ in top_n[user_id]:
-            recommend_records.append(recommendAPI.RecommendRecord(user_id=user_id, movie_id=str(movie_id)))
-    # print(recommend_records[0])
+        for movie_id, rating in top_n[user_id]:
+            recommend_records.append(
+                recommendAPI.RecommendRecord(user_id=user_id, movie_id=str(movie_id), rating=rating))
     recommendAPI.add(records=recommend_records)
+    print("finished LFM")
+
+
+def run_KNN(train_set, movie_set):
+    print("run_KNN")
+
+    model = KNNBasic(sim_options={
+        "name": "cosine",
+        "user_based": False,
+    })
+    model_name = os.path.expanduser('./dump_files/knn_model')
+    train_set_file = os.path.expanduser('./dump_files/train_set')
+    model.fit(train_set)
+    surprise.dump.dump(model_name, model)
+    surprise.dump.dump(train_set_file, train_set)
+    print('dump finished')
 
 
 if __name__ == '__main__':
-    run_LFM()
+    enter()
