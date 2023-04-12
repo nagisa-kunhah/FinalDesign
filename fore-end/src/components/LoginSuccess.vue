9e29ba22648e8de8682745ccdfc626249eb209e6
nagisa <1434936049@qq.com>
2023-04-12 21:05:44 +0800

    feat: add LoginSuccess.vue but not use

---
+++ fore-end/src/components/LoginSuccess.vue
@@ -0,0 +1,42 @@
+<template>
+  <MyTitle></MyTitle>
+  <h1>登陆成功</h1>
+</template>
+
+<script>
+import MyTitle from "@/App.vue";
+
+export default {
+  name: "LoginSuccess",
+  components: {MyTitle},
+  created() {
+    this.threeGo()
+  },
+  methods:{
+    threeGo(){
+      const TIME_COUNT = 3;
+      if(!this.timer){
+        this.count = TIME_COUNT;
+        this.show = false;
+        this.timer = setInterval(()=>{
+          if(this.count > 0 && this.count <= TIME_COUNT){
+            this.count--;
+          }else{
+            this.show = true;
+            clearInterval(this.timer);
+            this.timer = null;
+            //跳转的页面写在此处
+            this.$router.push({
+              name:'home'
+            });
+          }
+        },1000)
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
