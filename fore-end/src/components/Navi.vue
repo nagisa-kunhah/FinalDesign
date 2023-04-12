9924bf8d0e0684a5af96ffc68cc942161517f9e3
nagisa <1434936049@qq.com>
2023-04-12 21:06:42 +0800

    refactor: use the data from app.vue

--- fore-end/src/components/Navi.vue
+++ fore-end/src/components/Navi.vue
@@ -10,62 +10,51 @@
       主站
     </el-menu-item>
     <div class="grow-user"></div>
-    <el-menu-item index="3" v-if="!login" @click="to_login">
+    <el-menu-item index="3" v-if="!this.$root.login" @click="to_login">
       <div class="my-menu-item" >登录</div>
     </el-menu-item>
-    <el-menu-item index="4" v-if="!login" @click="to_register">
+    <el-menu-item index="4" v-if="!this.$root.login" @click="to_register">
       <div class="my-menu-item">注册</div>
     </el-menu-item>
-    <el-menu-item index="3" v-if="login" style="height: 100%">
-      <div class="img-box" style="{display: flex;align-items: center;justify-content: center;height: 100%}">
-        <div class="img-show" :style="{backgroundImage:'url('+img+')'}">
+    <el-menu-item index="3" v-if="this.$root.login" style="height: 100%" @click="to_center">
+        <div class="img-box" style="{display: flex;align-items: center;justify-content: center;height: 100%}">
+            <div class="img-show" :style="{backgroundImage:'url('+img+')'}">
+            </div>
         </div>
-      </div>
+    </el-menu-item>
+    <el-menu-item index="4" v-if="this.$root.login" style="height: 100%" @click="logout">
+      <div class="my-menu-item" >登出</div>
     </el-menu-item>
   </el-menu>
 </template>
 
 <script>
 import axios from "axios";
-import login from "@/components/Login.vue";
 
 export default {
   name: "Navi",
   data(){
     return{
-      user_id:null,
-      login:false,
       activeIndex:'1',
       img:require('@/assets/logo.png'),
     }
   },
   created(){
-    this.set_login()
+    console.log('navi: to set login')
+    console.log('navi: set login!',this.$root.user_id,'  ',this.$root.login)
   },
   methods:{
-    to_root(){
-      this.$root.user_id=this.user_id
-    },
-    async set_login() {
-      let ret = localStorage.getItem("8080:userInfo")
-      if (ret == null) {
-        this.login = false
-      } else {
-        await this.check_jwt(ret)
-      }
-    },
-    async check_jwt(token) {
-      let data = {
-        "token": token,
-      }
-      await axios.post("http://localhost:8087/user/CheckLogin", data).then((res) => {
-        this.login = (!!res.data.response)
-        this.user_id=res.data.user_id
-        this.to_root()
-      })
+    to_center(){
+      this.$router.push({name:"UserCenter",query:{
+          user_id:this.user_id,
+        }})
     },
     to_home(){
       this.$router.push({name:'home'})
+    },
+    logout(){
+      localStorage.removeItem("8080:userInfo")
+      this.$router.go(0)
     },
     handleSelect(key, keyPath) {
       console.log(key, keyPath)
